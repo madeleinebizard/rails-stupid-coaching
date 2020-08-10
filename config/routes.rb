da49e779230d7f7ea6b385a9deb3decf7836e3f3
Madeleine Bizard <bizard.madeleine@gmail.com>
2020-08-10 14:40:01 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root          to: 'questions#ask',    as: :question
-  get 'answer', to: 'questions#answer', as: :answer
 end
